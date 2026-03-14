acca965087c429798253cbe7fb421527d56d8b72
Justadeveloper <137354738+Edischinho@users.noreply.github.com>
2026-03-14 18:42:05 -0300

    Create script.js

---
+++ Acervo-Biblioteca/JS/script.js
@@ -0,0 +1,49 @@
+const grid = document.getElementById("grid")
+const search = document.getElementById("search")
+
+let livros=[]
+
+async function carregar(){
+
+let res = await fetch("http://localhost:3000/livros")
+
+livros = await res.json()
+
+mostrar(livros)
+
+}
+
+function mostrar(lista){
+
+grid.innerHTML=""
+
+lista.forEach(livro=>{
+
+let div = document.createElement("div")
+
+div.className="livro"
+
+div.innerHTML=`
+
+<img src="http://localhost:3000/${livro.capa}">
+<p>${livro.titulo}</p>
+
+`
+
+grid.appendChild(div)
+
+})
+
+}
+
+search.addEventListener("input",()=>{
+
+let filtro = search.value.toLowerCase()
+
+let filtrados = livros.filter(l=>l.titulo.toLowerCase().includes(filtro))
+
+mostrar(filtrados)
+
+})
+
+carregar()
